9f6854fba76e40fa69973a160332355fe4a6e0ec
Owen Nicholson <nicholowen@ecs.vuw.ac.nz>
2021-05-16 11:53:17 +1200

    Add req methods to LogsServlet.java Add maven dependencies and plugins

--- src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
@@ -1,4 +1,29 @@
 package nz.ac.wgtn.swen301.a3.server;
 
-public class LogsServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LogsServlet extends HttpServlet {
+
+  //constructor with no parameters as per assignment
+  public LogsServlet(){}
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    super.doGet(req, resp);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    super.doPost(req, resp);
+  }
+
+  @Override
+  protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    super.doDelete(req, resp);
+  }
+
 }
